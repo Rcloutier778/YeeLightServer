06945c921ea121c8b031c64abf125c77af42a8ec
rcloutier778 <rcloutier778@gmail.com>
2021-04-11 11:59:43 -0400

    Fixed ez status/restart bash scripts

--- restart.sh
+++ restart.sh
@@ -2,7 +2,8 @@ pkill yeelightpython.py
 pkill yeelightServer.py
 echo "Vlads service is disabled, need to enable it once you get IP"
 sudo systemctl restart yeelight_server.service 
-sudo systemctl restart yeelight_phone_vlad.service
-sudo systemctl restart yeelight_phone_richard.service
+#sudo systemctl restart yeelight_phone_vlad.service
+#sudo systemctl restart yeelight_phone_richard.service
+sudo systemctl restart yeelight_lights_server.service
 sudo /etc/init.d/cron restart
 
