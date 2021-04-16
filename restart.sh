571f640f78ddb402c1ac845a8a71b947d2145702
Richard <rcloutier778@gmail.com>
2021-04-16 11:08:39 -0400

    Updated restart and status scripts

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
-pkill yeelightpython.py
-pkill yeelightServer.py
-echo "Vlads service is disabled, need to enable it once you get IP"
+#pkill yeelightpython.py
+#pkill yeelightServer.py
+#echo "Vlads service is disabled, need to enable it once you get IP"
 sudo systemctl restart yeelight_server.service 
 #sudo systemctl restart yeelight_phone_vlad.service
 #sudo systemctl restart yeelight_phone_richard.service
